32d473aae09732901d0f5e1c8b280bce14746b36
Adam B <cruxic@pm.me>
2019-12-04 21:40:25 -0800

    robin32 WIP

---
+++ server-side/src/robin32/robin32_test.go
@@ -0,0 +1,17 @@
+package robin32
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+	//"util"
+	"math/rand"
+	//"fixedpool"
+)
+
+func Test_Entry(t * testing.T) {
+	req := require.New(t)
+
+	rand.Seed(1)
+
+	req.True(true)
+}
